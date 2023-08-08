5b18a8aa9cbdffcf1d0b1e53f552220033668c0d
FrezFreedom <frez1374@gmail.com>
2023-08-09 02:00:16 +0330

    Refactor StringParser class

--- StringParser.php
+++ StringParser.php
@@ -5,7 +5,9 @@ class StringParser
 {
     public function giveNumbers(string $str): array
     {
+        $str = str_replace('\n', ',', $str);
         $str_number_list = explode(',', $str);
+        echo implode('-', $str_number_list) . PHP_EOL;
         $number_list = array_map('intval', $str_number_list);
         return $number_list;
     }
