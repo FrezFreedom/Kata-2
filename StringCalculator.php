154a524a75b8d1b3d6251c56e9c26e7569a6c4e7
FrezFreedom <frez1374@gmail.com>
2023-08-09 20:03:05 +0330

    Refactor code to pass the test

--- StringCalculator.php
+++ StringCalculator.php
@@ -7,6 +7,7 @@ class StringCalculator
     public function add(string $str): int
     {
         $stringParser = new StringParser();
+        $stringParser->checkExsitanceDelimiterAtEnd($str);
         $number_list = $stringParser->giveNumbers($str);
         $answer = 0;
         foreach($number_list as $number)
--- StringParser.php
+++ StringParser.php
@@ -7,8 +7,15 @@ class StringParser
     {
         $str = str_replace('\n', ',', $str);
         $str_number_list = explode(',', $str);
-        echo implode('-', $str_number_list) . PHP_EOL;
         $number_list = array_map('intval', $str_number_list);
         return $number_list;
+    }
+
+    public function checkExsitanceDelimiterAtEnd(string $str)
+    {
+        if(strlen($str)!=0 && !ctype_digit($str[-1]))
+        {
+            throw new InvalidArgumentException('Invalid String');
+        }
     }
 }
