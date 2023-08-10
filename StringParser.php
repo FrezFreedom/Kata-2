0688d57fda57f73ee30029c6d78c66c0644548a8
FrezFreedom <frez1374@gmail.com>
2023-08-10 11:02:38 +0330

    Refactor code to pass test

--- StringParser.php
+++ StringParser.php
@@ -7,16 +7,30 @@ class StringParser
     {
         $this->checkExsitanceDelimiterAtEnd($str);
         $str = str_replace('\n', ',', $str);
+
+        $this->checkContinuousDelimiter($str);
+
         $str_number_list = explode(',', $str);
         $number_list = array_map('intval', $str_number_list);
         return $number_list;
+    }
+
+    public function checkContinuousDelimiter($str)
+    {
+        if(strpos($str, ',,') !== false)
+            $this->throwException();
     }
 
     public function checkExsitanceDelimiterAtEnd(string $str)
     {
         if(strlen($str)!=0 && !ctype_digit($str[-1]))
         {
-            throw new InvalidArgumentException('Invalid String');
+            $this->throwException();
         }
+    }
+
+    public function throwException()
+    {
+        throw new InvalidArgumentException('Invalid String');
     }
 }
