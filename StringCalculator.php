f3eb57d63c4b39edb5376282cbfb818880aa5d2e
FrezFreedom <frez1374@gmail.com>
2023-08-10 15:53:31 +0330

    Refactor code to pass the tests

--- StringCalculator.php
+++ StringCalculator.php
@@ -4,15 +4,53 @@ require_once('StringParser.php');
 
 class StringCalculator
 {
-    public function add(string $str): int
+    const NEGETIVE_NUMBER_ERROR = 'Negative number(s) not allowed: ';
+    public function add(string $str): int|string
     {
         $stringParser = new StringParser();
         $number_list = $stringParser->giveNumbers($str);
+
+        if($this->hasNegetiveNumber($number_list))
+        {
+            return $this->negetiveNumberError($number_list);
+        }
+
         $answer = 0;
         foreach($number_list as $number)
         {
             $answer += $number;
         }
         return $answer;
+    }
+
+    private function negetiveNumberError(array $number_list): string
+    {
+        $error = self::NEGETIVE_NUMBER_ERROR;
+        $first = TRUE;
+        foreach($number_list as $number)
+        {
+            if($number < 0)
+            {
+                if($first)
+                {
+                    $error .= strval($number);
+                }
+                else
+                {
+                    $error .= ', ' . strval($number);
+                }
+            }
+        }
+        return $error;
+    }
+
+    private function hasNegetiveNumber(array $number_list): bool
+    {
+        foreach($number_list as $number)
+        {
+            if($number < 0)
+                return true;
+        }
+        return false;
     }
 }
